0b56f2b16dfcf431fc6863fc133128a7482862e0
Matt Krause <krause.matt@pm.me>
2022-06-09 22:34:57 -0400

    Styling calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import "./app.css";
+
 function App() {
-  return;
+  return (
+    <div className="calculator-frame">
+      <div className="output-window">
+        <div className="previous-entry">234,234</div>
+        <div className="current-entry">224,243,223,673</div>
+      </div>
+      <button className="two-col">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="two-col">=</button>
+    </div>
+  );
 }
 
 export default App;
